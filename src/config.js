f0113fdf92418a3011d6d53c127aeb3a6eda364d
ctomrp <109928707+ctomrp@users.noreply.github.com>
2024-03-10 16:30:54 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
 export const firebaseConfig = {
 
-    apiKey: process.env.API_KEY,
+    apiKey: `${process.env.API_KEY}`,
   
-    authDomain: process.env.AUTH_DOMAIN,
+    authDomain: `${process.env.AUTH_DOMAIN}`,
   
-    projectId: process.env.PROJECT_ID,
+    projectId: `${process.env.PROJECT_ID}`,
   
-    storageBucket: process.env.STORAGE_BUCKET,
+    storageBucket: `${process.env.STORAGE_BUCKET}`,
   
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    messagingSenderId: `${process.env.MESSAGING_SENDER_ID}`,
   
-    appId: process.env.APP_ID
+    appId: `${process.env.APP_ID}`
   
   };
   
